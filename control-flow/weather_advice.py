406a0c56cedd004a8f53f60c9368413191e4cf7d
Kamohelo <kamohelotshabalala@proton.me>
2025-09-17 22:57:32 +0200

    Mor mofifications on input prompt for weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-ask_weather = input(str"What's the weather like today? (sunny/rainy/cold): \n"))
+input("What's the weather like today? (sunny/rainy/cold): ")
 
 if ask_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
