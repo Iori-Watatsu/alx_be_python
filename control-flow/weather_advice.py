14885b7d19c9e139a67a610fe2f2459a60ae6048
Kamohelo <kamohelotshabalala@proton.me>
2025-09-17 22:59:44 +0200

    Change on the if and elif statments on weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 input("What's the weather like today? (sunny/rainy/cold): ")
 
-if ask_weather == "sunny":
+if input == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif ask_weather == "rainy":
+elif input == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif ask_weather == "cold":
+elif input == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
