af02381e724d056d0d8c111e47ebb0ee5097b098
Kamohelo <kamohelotshabalala@proton.me>
2025-09-23 11:25:23 +0200

    Add .gitignore

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,25 @@
+def perform_operation(num1=float, num2=float, operation=str):
+    print("Arithmetic Operations")
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    operation = input("Enter the operation (add, subtract, multiply, divide): ")
+    match operation:
+        case "add":
+            result = num1 + num2
+            print("Result: ", result)
+        case "subtract":
+            result = num1 - num2
+            print("Result: ", result)
+        case "multiply":
+            result = num1 * num2
+            print("Result: ", result)
+        case "divide":
+            if num1 == 0:
+                print("Cannot divide by 0.")
+            else:
+                result = num1 / num2
+                print("Result: ", result)
+    return result
+perform_operation(num1=float, num2=float, operation=str)
