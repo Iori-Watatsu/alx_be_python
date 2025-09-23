0b02ef9bf7b0930f98a9820f12e2c23ead1506b6
Kamohelo <kamohelotshabalala@proton.me>
2025-09-23 13:50:37 +0200

    Initial commit of explore_datetime.py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    
+    now = datetime.now()
+    current_date = now.strftime('%y/%m/%d %H:%M:%S')
+    
+    print(current_date)
+display_current_datetime()
+
+prompt = int(input("Enter the number of days to add to the current date: "))
+def calculate_future_date():
+    
+    now = datetime.now()
+    days = timedelta(days=prompt)
+    future_date = now + days
+
+    print(future_date.strftime('%y/%m/%d'))
+calculate_future_date() 
+    
+
