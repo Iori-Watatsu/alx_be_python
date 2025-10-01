4b19b5dd982eb63b071354f5cea4c58a0bb06102
Kamohelo <kamohelotshabalala@proton.me>
2025-10-01 02:45:07 +0200

    Commit code changes in test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,25 +3,20 @@ from simple_calculator import SimpleCalculator
 
 
 class TestSimpleCalculator(unittest.TestCase):
-    """Unit tests for the SimpleCalculator class."""
 
     def setUp(self):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        """Test add method"""
         self.assertEqual(self.calc.add(2, 3), 5)
 
     def test_subtraction(self):
-        """Test subtract method"""
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
-    def test_multiply(self):
-        """Test multiply method"""
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
 
-    def test_divide(self):
-        """Test divide method"""
+    def test_division(self):
         self.assertEqual(self.calc.divide(6, 3), 2)
 
 
