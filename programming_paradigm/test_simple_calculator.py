b55a269f45a3c81712b8528e400a2a36a7d67677
Kamohelo <kamohelotshabalala@proton.me>
2025-10-01 02:43:54 +0200

    Commit code changes in test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,34 +9,20 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
+        """Test add method"""
         self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(-2, -3), -5)
-        self.assertEqual(self.calc.add(0, 5), 5)
 
     def test_subtraction(self):
-        self.assertEqual(self.calc.subtract(10, 5), 5)
-        self.assertEqual(self.calc.subtract(5, 10), -5)
-        self.assertEqual(self.calc.subtract(-5, -2), -3)
-        self.assertEqual(self.calc.subtract(0, 5), -5)
+        """Test subtract method"""
+        self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(3, 4), 12)
-        self.assertEqual(self.calc.multiply(-3, 4), -12)
-        self.assertEqual(self.calc.multiply(-3, -4), 12)
-        self.assertEqual(self.calc.multiply(0, 99), 0)
+        """Test multiply method"""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
 
     def test_divide(self):
-        # normal division
-        self.assertEqual(self.calc.divide(10, 2), 5)
-        # use almost equal for float results
-        self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
-        self.assertEqual(self.calc.divide(-10, 2), -5)
-        self.assertEqual(self.calc.divide(-10, -2), 5)
-        self.assertEqual(self.calc.divide(0, 5), 0)
-        # division by zero should return None
-        self.assertIsNone(self.calc.divide(10, 0))
-        self.assertIsNone(self.calc.divide(0, 0))
+        """Test divide method"""
+        self.assertEqual(self.calc.divide(6, 3), 2)
 
 
 if __name__ == "__main__":
